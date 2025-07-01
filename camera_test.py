1d1ea57c88bd2e3b971dbd5fe680d4f520996bca
Eli Lechien <elilechien@mac.local.meter>
2025-07-01 13:45:39 -0700

    Camera Test

--- camera_test.py
+++ camera_test.py
@@ -3,6 +3,36 @@ import subprocess
 from datetime import datetime
 import time
 import os
+from PIL import Image
+from pylibdmtx.pylibdmtx import decode
+import re
+
+def parse_digikey_data_matrix(raw: str):
+    # Remove the header
+    if raw.startswith("[)>06"):
+        raw = raw[5:]
+
+    # Split on ASCII Group Separator or Record Separator
+    # (\x1d = GS, \x1e = RS, \x04 = EOT)
+    fields = re.split(r'[\x1d\x1e\x04]+', raw)
+
+    result = {}
+
+    for field in fields:
+        if field.startswith("P") and not field.startswith("1P") and not field.startswith("30P"):
+            result["digi_key_pn"] = field[1:]
+        elif field.startswith("1P"):
+            result["mfr_pn"] = field[2:]
+        elif field.startswith("Q"):
+            result["qty"] = field[1:]
+        elif field.startswith("1T"):
+            result["lot_code"] = field[2:]
+        elif field.startswith("9D"):
+            result["date_code"] = field[2:]
+        elif field.startswith("12Z"):
+            result["mid"] = field[3:]
+
+    return result
 
 try:
     while True:
@@ -19,6 +49,18 @@ try:
         ])
 
         print(f"Saved {filename}")
+        img = Image.open(filename)
+        results = decode(img)
+
+        raw = results[0].data.decode("utf-8")
+
+        # Match known field prefixes
+        result = parse_digikey_data_matrix(raw)
+        if("digi_key_pn" in result):
+            print('DIGIKEY DATAMATRIX DETECTED.')
+            print(result)
+        else:
+            print("No Data Matrix found.")
 
         # Load and show image using OpenCV
         img = cv2.imread(filename)
